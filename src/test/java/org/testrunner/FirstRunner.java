ea5f0f25240ec3cdfd5123a45d4d9c4cff858a37
Kabilan-12taste <kabilan.mohanraj@12taste.com>
2025-03-20 14:19:10 +0530

    Fixed Cucumber JSON report path

--- src/test/java/org/testrunner/FirstRunner.java
+++ src/test/java/org/testrunner/FirstRunner.java
@@ -1,34 +1,38 @@
 package org.testrunner;
 
+import java.io.File;
+import org.junit.BeforeClass;
 import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 import org.stepdefinitions.JVMReport;
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-	    features = "C:\\Users\\Lenovo\\eclipse-workspace\\1-2-taste\\src\\test\\resources\\FeatureFiles\\LoginwithValid.feature",
-	    glue = "org.stepdefinitions",
-	    tags = "not @skip",
-	    plugin = {
-	        "pretty",
-	        "html:target/cucumber-reports/cucumber-html-report",
-	        "json:target/cucumber-reports/cucumber.json",  // Ensure this is correct
-	        "junit:target/JunitReport/Junit.xml"
-	    },
-	    monochrome = true
-	)
-
+    features = "C:\\Users\\Lenovo\\eclipse-workspace\\1-2-taste\\src\\test\\resources\\FeatureFiles\\LoginwithValid.feature",
+    glue = "org.stepdefinitions",
+    tags = "not @skip",
+    plugin = {
+        "pretty",
+        "html:target/cucumber-reports/cucumber-html-report",
+        "json:target/cucumber-reports/cucumber.json",
+        "junit:target/JunitReport/Junit.xml"
+    },
+    monochrome = true
+)
 public class FirstRunner {
 
-	@AfterClass
-	public static void jvm() {
+    @BeforeClass
+    public static void setup() {
+        File reportDir = new File("target/cucumber-reports");
+        if (!reportDir.exists()) {
+            reportDir.mkdirs();  // Create the directory if it doesn’t exist
+        }
+    }
 
-		JVMReport.generateJVMReport("target/cucumber-reports/cucumber.json");
-
-
-
-	}
+    @AfterClass
+    public static void jvm() {
+        JVMReport.generateJVMReport("target/cucumber-reports/cucumber.json");
+    }
 }
