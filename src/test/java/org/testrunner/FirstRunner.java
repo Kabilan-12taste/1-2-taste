9d14f959a138aa86d0eb3a85dc2198f724f4053f
Kabilan-12taste <kabilan.mohanraj@12taste.com>
2025-03-20 17:12:30 +0530

    Fixed Cucumber JSON report path

--- src/test/java/org/testrunner/FirstRunner.java
+++ src/test/java/org/testrunner/FirstRunner.java
@@ -10,17 +10,18 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features = "src/test/resources/FeatureFiles/LoginwithValid.feature",
-    glue = "org.stepdefinitions",
-    tags = "not @skip",
-    plugin = {
-        "pretty",
-        "html:target/cucumber-reports/index.html",
-        "json:target/cucumber-reports/cucumber.json",
-        "junit:target/JunitReport/Junit.xml"
-    },
-    monochrome = true
-)
+	    features = "src/test/resources/FeatureFiles/LoginwithValid.feature", // Path to feature files
+	    glue = "org.stepdefinitions", // Path to step definitions
+	    tags = "not @skip", // If you have any specific tags to include/exclude
+	    plugin = {
+	        "pretty",
+	        "html:target/cucumber-reports/index.html", // HTML report
+	        "json:target/cucumber-reports/cucumber.json", // JSON report
+	        "junit:target/JunitReport/Junit.xml" // JUnit report (optional)
+	    },
+	    monochrome = true
+	)
+
 public class FirstRunner {
 
 	   @AfterClass
