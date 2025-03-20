f335e5404b544591bacd038eb7fb964a18997899
Kabilan-12taste <kabilan.mohanraj@12taste.com>
2025-03-20 16:43:03 +0530

    Fixed Cucumber JSON report path

--- src/test/java/org/stepdefinitions/JVMReport.java
+++ src/test/java/org/stepdefinitions/JVMReport.java
@@ -1,26 +1,32 @@
 package org.stepdefinitions;
 
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.masterthought.cucumber.Configuration;
-import net.masterthought.cucumber.ReportBuilder;
-
 public class JVMReport {
-    public static void generateJVMReport(String jsonpath) {
-        File f = new File("target/JVMReport");  // Store reports inside target/
+    public static void generateJVMReport(String jsonPath) {
+        // Directory where the reports will be saved
+        File reportOutputDirectory = new File("target/cucumber-reports");
+        
+        // List to hold the path of the JSON files
+        List<String> jsonFiles = new ArrayList<>();
+        
+        // Add the path to the JSON file(s)
+        jsonFiles.add(jsonPath);
 
-        Configuration c = new Configuration(f, "1-2 Taste - Automation Report");
-        c.addClassifications("OS Name", "Windows");
-        c.addClassifications("OS Version", "10");
-        c.addClassifications("Browser", "Chrome");
-        c.addClassifications("Project_Name", "1-2 Taste");
+        // Configuration for the report
+        Configuration config = new Configuration(reportOutputDirectory, "12Taste B2B");
+        config.addClassifications("Platform", "Windows");
+        config.addClassifications("Browser", "Chrome");
+        config.addClassifications("Environment", "QA");
 
-        List<String> li = new ArrayList<>();
-        li.add(jsonpath);
-
-        ReportBuilder r = new ReportBuilder(li, c);
-        r.generateReports();
+        // Create a report builder
+        ReportBuilder reportBuilder = new ReportBuilder(jsonFiles, config);
+        
+        // Generate the report
+        reportBuilder.generateReports();
     }
 }
