cc11f681403348e4828957c22460393a6061d35e
Kabilan-12taste <kabilan.mohanraj@12taste.com>
2025-03-20 12:02:37 +0530

    Push to Upstream

--- src/test/java/org/testrunner/FirstRunner.java
+++ src/test/java/org/testrunner/FirstRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "C:\\Users\\Lenovo\\eclipse-workspace\\1-2-taste\\src\\test\\resources\\FeatureFiles\\Quote.feature",
+@CucumberOptions(features = "C:\\Users\\Lenovo\\eclipse-workspace\\1-2-taste\\src\\test\\resources\\FeatureFiles\\LoginwithValid.feature",
 glue = "org.stepdefinitions", tags = "not @skip", plugin = {"html:HtmlReport", "junit:JunitReport\\Junit.xml", "json:JsonReport\\Jsonreport.json"})
 
 public class FirstRunner {
