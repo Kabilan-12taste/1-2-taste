fbcd2f0c5b26144c8780396d56d6e2051c1ca882
Kabilan-12taste <kabilan.mohanraj@12taste.com>
2025-03-20 17:18:48 +0530

    Fixed Cucumber JSON report path

--- src/test/java/org/testrunner/FirstRunner.java
+++ src/test/java/org/testrunner/FirstRunner.java
@@ -9,23 +9,18 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-	    features = "src/test/resources/FeatureFiles/LoginwithValid.feature", // Path to feature files
-	    glue = "org.stepdefinitions", // Path to step definitions
-	    tags = "not @skip", // If you have any specific tags to include/exclude
-	    plugin = {
-	        "pretty",
-	        "html:target/cucumber-reports/index.html", // HTML report
-	        "json:target/cucumber-reports/cucumber.json", // JSON report
-	        "junit:target/JunitReport/Junit.xml" // JUnit report (optional)
-	    },
-	    monochrome = true
-	)
+@CucumberOptions(features = "src/test/resources/FeatureFiles/LoginwithValid.feature", // Path to feature files
+		glue = "org.stepdefinitions", // Path to step definitions
+		tags = "not @skip", // If you have any specific tags to include/exclude
+		plugin = { "pretty", "html:./target/cucumber-reports/index.html", // Explicit relative path
+				"json:./target/cucumber-reports/cucumber.json", "junit:./target/JunitReport/Junit.xml" },
+
+		monochrome = true)
 
 public class FirstRunner {
 
-	   @AfterClass
-	    public static void jvmReport() {
-	        JVMReport.generateJVMReport("target/cucumber-reports/cucumber.json");
-	    }
+	@AfterClass
+	public static void jvmReport() {
+		JVMReport.generateJVMReport("target/cucumber-reports/cucumber.json");
+	}
 }
