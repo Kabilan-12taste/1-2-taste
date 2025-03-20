dcf1e89147625ff8bf25a6ac00dee54b11e7c02b
Kabilan-12taste <kabilan.mohanraj@12taste.com>
2025-03-20 12:50:38 +0530

    Updated FirstRunner, pom.xml, and added test reports

--- src/test/java/org/testrunner/FirstRunner.java
+++ src/test/java/org/testrunner/FirstRunner.java
@@ -8,17 +8,25 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "C:\\Users\\Lenovo\\eclipse-workspace\\1-2-taste\\src\\test\\resources\\FeatureFiles\\LoginwithValid.feature",
-glue = "org.stepdefinitions", tags = "not @skip", plugin = {"html:HtmlReport", "junit:JunitReport\\Junit.xml", "json:JsonReport\\Jsonreport.json"})
+@CucumberOptions(
+	    features = "C:\\Users\\Lenovo\\eclipse-workspace\\1-2-taste\\src\\test\\resources\\FeatureFiles\\LoginwithValid.feature",
+	    glue = "org.stepdefinitions",
+	    tags = "not @skip",
+	    plugin = {
+	        "pretty",
+	        "html:target/HtmlReport",
+	        "json:target/cucumber-reports/CucumberReport.json",  // Ensure this is correct
+	        "junit:target/JunitReport/Junit.xml"
+	    }
+	)
 
 public class FirstRunner {
 
 	@AfterClass
 	public static void jvm() {
-		
-		JVMReport.generateJVMReport("C:\\Users\\Lenovo\\eclipse-workspace\\1-2-taste\\JsonReport\\Jsonreport.json");
-	
+
+		JVMReport.generateJVMReport("target/JVMReport");
+
+
 	}
 }
-
-
