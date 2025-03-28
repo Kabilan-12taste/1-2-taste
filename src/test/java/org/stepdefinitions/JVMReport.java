a769e51c18aa34c261f4e8bcbbd525a08940afec
Kabilan-12taste <kabilan.mohanraj@12taste.com>
2025-03-28 12:09:33 +0530

    updated all

--- src/test/java/org/stepdefinitions/JVMReport.java
+++ src/test/java/org/stepdefinitions/JVMReport.java
@@ -2,38 +2,23 @@ package org.stepdefinitions;
 
 import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.ReportBuilder;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 public class JVMReport {
-    public static void generateJVMReport(String jsonPath) {
-        // Directory where the reports will be saved
-        File reportOutputDirectory = new File("target/cucumber-reports");
-        
-        // List to hold the path of the JSON files
-        List<String> jsonFiles = new ArrayList<>();
-        
-        // Add the path to the JSON file(s)
-        File jsonFile = new File(jsonPath);
-        if (!jsonFile.exists()) {
-            System.out.println("ERROR: Cucumber JSON file not found! Path: " + jsonPath);
-            return;
-        }
-
-        jsonFiles.add(jsonPath);
-
-        // Configuration for the report
-        Configuration config = new Configuration(reportOutputDirectory, "12Taste B2B");
+    public static void generateJVMReport(String jsonFilePath) {
+        File reportOutputDirectory = new File("target/cucumber-html-reports");
+        Configuration config = new Configuration(reportOutputDirectory, "12Taste Automation");
         config.addClassifications("Platform", "Windows");
         config.addClassifications("Browser", "Chrome");
         config.addClassifications("Environment", "QA");
 
-        // Create a report builder
+        List<String> jsonFiles = new ArrayList<>();
+        jsonFiles.add(jsonFilePath);
+
         ReportBuilder reportBuilder = new ReportBuilder(jsonFiles, config);
-        
-        // Generate the report
         reportBuilder.generateReports();
-        System.out.println("JVM Report successfully generated at: " + reportOutputDirectory.getAbsolutePath());
     }
 }
--- src/test/java/org/testrunner/FirstRunner.java
+++ src/test/java/org/testrunner/FirstRunner.java
@@ -8,14 +8,15 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features = "C:\\Users\\Lenovo\\eclipse-workspace\\1-2-taste\\src\\test\\resources\\FeatureFiles\\LoginwithValid.feature",
+    features = "src/test/resources/FeatureFiles/LoginwithValid.feature", // Relative path
     glue = "org.stepdefinitions",
     tags = "not @skip",
     plugin = {
         "pretty",
-        "html:target/cucumber-reports/index.html",
-        "json:target/cucumber-reports/Cucumber.json",
-        "junit:target/cucumber-reports/Cucumber.xml"
+        "html:target/cucumber-reports/CucumberReport.html",  
+        "json:target/cucumber-reports/CucumberReport.json",
+        "junit:target/cucumber-reports/CucumberReport.xml",
+        "rerun:target/failed_scenarios.txt"
     },
     monochrome = true
 )
@@ -23,6 +24,6 @@ public class FirstRunner {
     @AfterClass
     public static void generateReport() {
         System.out.println("Generating JVM Report...");
-        JVMReport.generateJVMReport("target/cucumber-reports/Cucumber.json");
+        JVMReport.generateJVMReport("target/cucumber-reports/CucumberReport.json");
     }
 }
