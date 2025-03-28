3beeffe48d18040c65bf4f76b7ee0e2949e2b5d3
Kabilan-12taste <kabilan.mohanraj@12taste.com>
2025-03-28 17:28:18 +0530

    update

--- src/test/java/org/testrunner/FirstRunner.java
+++ src/test/java/org/testrunner/FirstRunner.java
@@ -8,13 +8,15 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features = "src/test/resources/FeatureFiles/LoginwithValid.feature", // Relative path
+    features = "src/test/resources/FeatureFiles", // Relative path
     glue = "org.stepdefinitions",
     tags = "not @skip",
     plugin = {
         "pretty",
-        "html:target/cucumber-reports/index.html",
-        "json:target/cucumber-reports/CucumberReport.json"
+        "html:target/cucumber-reports/CucumberReport.html",  
+        "json:target/cucumber-reports/CucumberReport.json",
+        "junit:target/cucumber-reports/CucumberReport.xml",
+        "rerun:target/failed_scenarios.txt"
     },
     monochrome = true
 )
